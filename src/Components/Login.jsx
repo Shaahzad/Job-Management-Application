0743dac4eaeeb7639872114ffcb219331e9befd9
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-01-19 14:59:11 +0500

    login form complete

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,45 +1,115 @@
-import React, { useState } from 'react'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Button, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import {
+  Button,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailerror, setEmailError] = useState("");
+  const [passworderror, setPasswordError] = useState("");
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const hardcodeEmail = "admin@example.com";
+  const hardcodePassword = "password123";
 
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-  
-    const handleMouseUpPassword = (event) => {
-      event.preventDefault();
-    };
+  const LoginHandler = () => {
+    if (!email && !password) {
+      setEmailError("Email cannot be empty");
+      setPasswordError("Password cannot be empty");
+    } else if (!email) {
+      setEmailError("Email cannot be empty");
+    } else if (!password) {
+      setPasswordError("Password cannot be empty");
+    } else if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long");
+    } else if (email === hardcodeEmail && password === hardcodePassword) {
+      setPassword("");
+      setEmail("");
+      alert("Login Successful");
+    } else if (email !== hardcodeEmail) {
+      setEmailError("Invalid email");
+    } else if (password !== hardcodePassword) {
+      setPasswordError("Invalid password");
+    }
+    
+  };
 
-    return (
-        <Card sx={{ maxWidth: 360, margin: 'auto', marginTop: '50px', padding: '10px' }}>
-            <CardContent>
-              <Typography gutterBottom sx={{fontSize: '40px', fontWeight: 'bold'}}>
-                Recruiter Login
-                <Typography variant="body2" sx={{marginBottom: '50px', color: 'gray'}}>
-                Manage job postings effortlessly. Sign in to access your dashboard and find the best candidates.
-                </Typography>
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card
+      sx={{
+        maxWidth: { md: "500px", xs: "300px" },
+        margin: "auto",
+        marginTop: "100px",
+        padding: "10px",
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+      }}
+    >
+      <CardContent>
+        <Typography
+          gutterBottom
+          sx={{
+            fontSize: { md: "40px", xs: "24px" },
+            fontWeight: "bold",
+            marginBottom: "0px",
+          }}
+        >
+          Recruiter Login
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{
+            marginBottom: "20px",
+            color: "gray",
+            fontSize: { md: "16px", xs: "12px" },
+          }}
+        >
+          Manage job postings effortlessly. Sign in to access your dashboard and
+          find the best candidates.
+        </Typography>
+        <div style={{ display: "flex", flexDirection: "column", gap: "14px"}}>
+          <TextField
+            onChange={(e) => setEmail(e.target.value)}
+            id="outlined-basic"
+            placeholder="Enter Email Address"
+            sx={{ width: "100%" }}
+            style={{ border: emailerror ? "1px solid red" : "none", borderRadius: "4px" }}
+            />
+            {emailerror && (
+              <Typography variant="body2" sx={{ color: "red", fontSize: "14px" }}>
+                {emailerror}
               </Typography>
-               <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
-              <TextField id="outlined-basic" placeholder='Enter Email Address' sx={{width:'100%'}}/>
-              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            )}
           <OutlinedInput
+            onChange={(e) => setPassword(e.target.value)}
             id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
+            placeholder="Enter Password"
+            type={showPassword ? "text" : "password"}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
                   aria-label={
-                    showPassword ? 'hide the password' : 'display the password'
+                    showPassword ? "hide the password" : "display the password"
                   }
                   onClick={handleClickShowPassword}
                   onMouseDown={handleMouseDownPassword}
@@ -50,15 +120,30 @@ const Login = () => {
                 </IconButton>
               </InputAdornment>
             }
-            label="Password"
-          />              
-          <Button variant="contained" sx={{backgroundColor: '#3f51b5', color: 'white',borderRadius: '30px', padding: '12px', fontWeight: 'bold'}}>
-               login
-              </Button>
-               </div>
-            </CardContent>
-        </Card>
-      );
-}
+          />
+          {passworderror && (
+            <Typography variant="body2" sx={{ color: "red", fontSize: "14px" }}>
+              {passworderror}
+            </Typography>
+          )}
+          <Button
+            onClick={LoginHandler}
+            variant="contained"
+            sx={{
+              backgroundColor: "var(--primary-color)",
+              color: "white",
+              borderRadius: "30px",
+              padding: "12px",
+              fontWeight: "bold",
+              marginTop: "20px",
+            }}
+          >
+            login
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default Login
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './Redux/Store.js'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 createRoot(document.getElementById('root')).render(
    <Provider store={store}>
